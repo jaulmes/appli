382f8a1966748928e8ccb89854a890a135d51ccc
jaulmes <anoudemj@gmail.com>
2024-12-13 02:17:28 +0100

    update aprovisionnement

--- app/Livewire/CatalogueAprovisionnementProduit.php
+++ app/Livewire/CatalogueAprovisionnementProduit.php
@@ -7,6 +7,19 @@ use Livewire\Component;
 
 class CatalogueAprovisionnementProduit extends Component
 {
+    //ajout de nouveau produit dans le panier
+    public function ajouterPanier($id){
+        $produits = Produit::find($id);
+        
+        \Cart::add($produits->id, $produits->name, $produits->price, 1,array())
+                    ->associate($produits);
+
+        /**
+         * emmission d'un evenement apres ajout du produit 
+         * dans le panier pour ecouter et rafraichir le composant mon panier
+         * */
+        $this->dispatch('ProduitAjoute');
+    }
     public function render()
     {
         $produits = Produit::all();
