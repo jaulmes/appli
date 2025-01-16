df43c630101bbe23311b8fcd766531ccf9308f1b
jaulmes <anoudemj@gmail.com>
2025-01-16 04:36:34 +0100

    ajout du bouton de la cloche de notification

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -45,6 +45,54 @@
 </head>
 <body class="hold-transition  sidebar-mini layout-fixed  ">
     <div class="wrapper">
+      <!-- Navbar -->
+      <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+          <!-- Left navbar links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+              <a href="{{ route('panier.index')}}" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+              <form action="{{route('logout')}}" method="post" >
+                  @csrf
+                  <button type="submit">
+                  <i class="bi bi-power"></i>
+                      deconnexion
+                  </button>
+              </form>
+            </li>
+          </ul>
+
+          <!-- Right navbar links -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Notifications Dropdown Menu -->
+            <li class="nav-item dropdown">
+              <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="bi bi-bell"></i>
+                <span class="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-danger">
+                  0
+                </span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">15 Notifications</span>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-envelope mr-2"></i> 4 new messages
+                  <span class="float-right text-muted text-sm">3 mins</span>
+                </a>
+                
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+              </div>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.navbar -->
+
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             @include('dashboard.sidebar')
@@ -52,26 +100,6 @@
         </aside>
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="{{ route('panier.index')}}">ACCEUIL</a></li>
-                </ol>
-                <form action="{{route('logout')}}" method="post" >
-                    @csrf
-                    <button type="submit">
-                    <i class="bi bi-power"></i>
-                        deconnexion
-                    </button>
-                </form>
-              </div><!-- /.col -->
-            </div><!-- /.row -->
-          </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
         
         <!-- Main content -->
         <section class="content">
