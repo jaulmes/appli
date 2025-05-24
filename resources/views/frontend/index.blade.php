4ec1e459865372aa3009abcf2abe6ce5ca9c8939
jaulmes <anoudemj@gmail.com>
2025-05-24 11:00:57 +0100

    ajout du pixel de solergy

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -141,7 +141,7 @@
       position: fixed;
       bottom: 7.5rem;      /* légèrement au-dessus de l’orange (orange est à 6rem) */
       right: 2rem;
-      background-color:rgb(230, 183, 44); /* jaune Bootstrap */
+      background-color:rgb(244, 233, 28); /* jaune Bootstrap */
       color: #fff;
       width: 2.5rem;
       height: 2.5rem;
@@ -200,19 +200,19 @@
 
 <!-- Meta Pixel Code -->
 <script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '681776711126337');
-  fbq('track', 'PageView');
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '9916049995153025');
+fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=681776711126337&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=9916049995153025&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
 </head>
