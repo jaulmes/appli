4f54f881401231d8b719af1ac9c447a7bfa952ef
jaulmes <anoudemj@gmail.com>
2025-05-30 18:02:50 +0100

    recherche dans les packs

--- app/Livewire/CataloguePack.php
+++ app/Livewire/CataloguePack.php
@@ -2,13 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Models\Categori;
 use App\Models\Produit;
 use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
 class CataloguePack extends Component
 {
-    public $cart = [], $new_price = [], $quantity = [], $categories = [], $produits = [];
+    public $cart = [], $new_price = [], $quantity = [], 
+            $categories = [], $produits = [], $categori,
+            $query;
+
+    public function update_query(){
+        //$word = $this->query;
+        $word = '%' . $this->query . '%';
+        $this->produits = Produit::where('name', 'like', '%'. $word . '%')
+                                    ->orWhere('description', 'like', '%'. $word . '%')
+                                    ->get();
+        $this->cart = Session::get('cart', []); 
+    }
+
+    //filtre des produits par categories
+    public function filtreProduit($id){
+        
+        $categoris = Categori::find( $id);
+        $this->categori = $categoris->titre;
+        $this->produits = Produit::where('categori_id', $id)
+                                    ->orderBy('name', 'asc')
+                                    ->get();
+        $this->cart = Session::get('cart', []); 
+    }
 
     public function addToCart($id)
     {
@@ -34,6 +57,7 @@ class CataloguePack extends Component
     }
 
     public function mount(){
+        $this->categories = Categori::all();
         $this->produits = Produit::all();
         $this->cart = session()->get('cart', []);
     }
